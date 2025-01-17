14bb62c199e72747fd99e78fe4d46b8d4b02748d
sandeep18p <sandeep.pansari98@gmail.com>
2025-01-17 16:45:40 +0530

    login_backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -31,4 +31,31 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email } });
+  
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const userData = user.get();
+
+if (userData.password !== password) {
+  return res.status(401).json({ message: 'User not authorized' });
+}
+
+
+  
+    console.log(userData)
+  
+
+    return res.status(200).json({ message: 'User login successful' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
 module.exports = router;
